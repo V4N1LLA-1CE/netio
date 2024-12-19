93640d01f486b58be49663fda89255753fd32b57
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-19 20:54:07 +1100

    test: recreate proper test cases for Write()

--- netio_test.go
+++ netio_test.go
@@ -1,47 +1,108 @@
-package netio_test
+package netio
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-
-	"github.com/V4N1LLA-1CE/netio"
+	"slices"
+	"testing"
 )
 
-// ExampleWrite demonstrates how to use the Write function with custom header configurations.
-func ExampleWrite() {
-	w := httptest.NewRecorder()
+func TestWrite(t *testing.T) {
+	// structs for testing
+	type TestData struct {
+		Name       string `json:"name"`
+		Age        int    `json:"age"`
+		Experience bool   `json:"experience"`
+	}
 
-	// create custom header
-	headers := http.Header{}
-	headers.Set("X-Custom-Header", "some-value")
-	headers["X-Many-Values"] = []string{"value1", "value2", "value3"}
-
-	headers.Add("X-Allowed-Methods", "GET")
-	headers.Add("X-Allowed-Methods", "POST")
-	headers.Add("X-Allowed-Methods", "PUT")
-	headers.Add("X-Allowed-Methods", "OPTIONS")
-
-	data := netio.Envelope{
-		"status": "success",
-		"user": map[string]any{
-			"id":   1,
-			"name": "Test User",
+	tests := []struct {
+		name       string
+		status     int
+		data       Envelope
+		headers    http.Header
+		wantErr    bool
+		wantStatus int
+	}{
+		{
+			name:       "simple write with empty header",
+			status:     http.StatusOK,
+			data:       Envelope{"message": "success"},
+			headers:    nil,
+			wantErr:    false,
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:   "complex write with headers",
+			status: http.StatusOK,
+			data: Envelope{"user": TestData{
+				Name:       "Jack",
+				Age:        42,
+				Experience: false,
+			}},
+			headers: http.Header{
+				"X-Many-Headers":  []string{"X-Do-Something", "X-Header-2", "X-Header-3"},
+				"X-Single-Header": []string{"X-Do-Not-Cache"},
+			},
+			wantErr:    false,
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "fail write with invalid data",
+			status:     http.StatusInternalServerError,
+			data:       Envelope{"data": func() {}},
+			headers:    nil,
+			wantErr:    true,
+			wantStatus: http.StatusInternalServerError,
 		},
 	}
 
-	err := netio.Write(w, http.StatusOK, data, headers)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
 
-	// print headers
-	fmt.Println("Single header:", w.Header().Get("X-Custom-Header"))
-	fmt.Println("Multiple headers (first method):", w.Header()["X-Many-Values"])
-	fmt.Println("Multiple headers (second method):", w.Header()["X-Allowed-Methods"])
-	// Output:
-	// Single header: some-value
-	// Multiple headers (first method): [value1 value2 value3]
-	// Multiple headers (second method): [GET POST PUT OPTIONS]
+			err := Write(w, test.status, test.data, test.headers)
+
+			hasErr := false
+			if err != nil {
+				hasErr = true
+			}
+
+			if hasErr != test.wantErr {
+				t.Errorf("Write() error = %v, wantErr %v", err, test.wantErr)
+			}
+
+			// make sure status is set
+			if w.Code != test.wantStatus {
+				t.Errorf("Write() code = %v, want %v", w.Code, test.wantStatus)
+			}
+
+			// make sure application/json is always set
+			if w.Header().Get("Content-Type") != "application/json" {
+				t.Error("Write() did not set Content-Type header to application/json")
+			}
+
+			// check if header values are the same
+			if test.headers != nil {
+				for key, values := range test.headers {
+					got := w.Header()[key]
+					if !slices.Equal(got, values) {
+						t.Errorf("Write() headers don't match what was given")
+					}
+				}
+			}
+
+			// check if json encoded by Write can be decoded without errors
+			if !test.wantErr {
+				var gotData Envelope
+				if err := json.NewDecoder(w.Body).Decode(&gotData); err != nil {
+					t.Errorf("Write() invalid JSON response")
+				}
+			}
+		})
+	}
 }
+
+// func TestRead(t *testing.T) {
+//
+// }
