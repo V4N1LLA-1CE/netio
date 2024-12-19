25115c3cc154959970ae0baeebc044964d05d98f
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-19 23:09:42 +1100

    refactor: change example filename and better docs

--- examples/read_write_example.go
+++ examples/read_write_example.go
@@ -22,31 +22,30 @@ func main() {
 }
 
 func exampleHandler(w http.ResponseWriter, r *http.Request) {
+	// This struct determines what you want to read from the response.
+	// If you set a field on the struct and it doesn't exist in the request, it will
+	// default to a 'falsey' value
+	//
+	// If there is a field in the request which isn't defined on
+	// this struct, it will return an error
 	var input struct {
 		Username string         `json:"username"`
 		UserData map[string]any `json:"user_data"`
 	}
 	// read data into input struct
-	// netio.Read() will
 	err := netio.Read(w, r, &input)
 	if err != nil {
 		// handle error
 		log.Fatal(err)
 	}
-	// write response
-	//
-	// headers can be set to  nil for netio.Write()
-	// if no further configuration is needed
+	// Headers can be set to  nil for netio.Write()
+	// if no further configuration is needed.
 	headers := http.Header{
 		"X-Some-Header": []string{"Value-1", "Value-2"},
 		"X-API-Version": []string{"1.0"},
 	}
 	headers.Add("X-New-Header", "New-Header-Value")
-	// netio.Write() will automatically set json headers (can be overriden by custom header)
-	//
-	// any type of data to be written must be wrapped by netio.Envelope
-	// which is just a map[string]any
-	// this ensures good JSON response structures
+	// netio.Write() will automatically set json headers (can be overriden by custom headers)
 	err = netio.Write(w, http.StatusOK, netio.Envelope{"example response": input}, headers)
 	if err != nil {
 		// handle error
